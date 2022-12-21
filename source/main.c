68e3ce65a7282ab99a9ef108258aba5f428cc894
rdouda <Zero_Drive@outlook.com>
2022-12-21 12:28:02 +0100

    remove t_add

--- source/main.c
+++ source/main.c
@@ -3,37 +3,8 @@
 Student students[MAX_STUDENTS];
 int students_count = 0;
 
-void t_add_students(Student students[], int* size) {
-	Student s1 = { 4, 2, 12.4, "rad" };
-	Student s2 = { 6, 2, 11.1, "7san" };
-	Student s3 = { 8, 1, 14.8, "geh" };
-	Student s4 = { 3, 6, 13.9, "puta" };
-
-	students[0].id = s1.id;
-	students[0].level = s1.level;
-	students[0].average = s1.average;
-	strcpy_s(students[0].name, sizeof(students[0].name), s1.name);
-
-	students[1].id = s2.id;
-	students[1].level = s2.level;
-	students[1].average = s2.average;
-	strcpy_s(students[1].name, sizeof(students[1].name), s2.name);
-
-	students[2].id = s3.id;
-	students[2].level = s3.level;
-	students[2].average = s3.average;
-	strcpy_s(students[2].name, sizeof(students[2].name), s3.name);
-
-	students[3].id = s4.id;
-	students[3].level = s4.level;
-	students[3].average = s4.average;
-	strcpy_s(students[3].name, sizeof(students[3].name), s4.name);
-
-	*size = *size + 4;
-}
-
 void main_menu() {
-	t_add_students(students, &students_count);
+	//t_add_students(students, &students_count);
 	while (1) {
 		system("cls");
 		int choice;
